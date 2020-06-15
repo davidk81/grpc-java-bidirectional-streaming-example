e54094c3b1b568eac2b5d67cad10842ff0524068
David Kuo <david.j.kuo@gmail.com>
2020-06-15 17:07:12 +0900

    try dual stream

--- src/main/java/GrpcExampleClient.java
+++ src/main/java/GrpcExampleClient.java
@@ -1,7 +1,6 @@
 import com.example.BiDirectionalExampleService;
 import com.example.BiDirectionalExampleService.RequestCall;
 import com.example.ExampleServiceGrpc;
-import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
 import io.grpc.*;
 import io.grpc.netty.NettyChannelBuilder;
@@ -12,7 +11,6 @@ import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
@@ -20,7 +18,7 @@ import java.util.concurrent.atomic.AtomicReference;
 public class GrpcExampleClient {
 
     static long startTime = 0;
-    static long totalBytes = 0;
+    static long totalSentBytes = 0;
 
     public static void main(String [] args) throws IOException, InterruptedException {
         String host = System.getProperty("host", "localhost");
@@ -34,28 +32,18 @@ public class GrpcExampleClient {
                 .flowControlWindow(640000000)
                 .build();
         ExampleServiceGrpc.ExampleServiceStub service = ExampleServiceGrpc.newStub(channel);
-        AtomicReference<StreamObserver<BiDirectionalExampleService.RequestCall>> requestObserverRef = new AtomicReference<>();
-        CountDownLatch finishedLatch = new CountDownLatch(1);
-        StreamObserver<com.example.BiDirectionalExampleService.RequestCall> observer = service.connect(new StreamObserver<BiDirectionalExampleService.ResponseCall>() {
-            long totalBytes = 0;
+        CountDownLatch finishedLatch = new CountDownLatch(2);
+
+        StreamObserver<BiDirectionalExampleService.ResponseCall> sharedObs = new StreamObserver<BiDirectionalExampleService.ResponseCall>() {
+            long totalRecvBytes = 0;
             @Override
             public void onNext(BiDirectionalExampleService.ResponseCall value) {
-                totalBytes += value.getSize();
-
-                if (totalBytes >= 1e9) {
-                    requestObserverRef.get().onCompleted();
-                    onCompleted();
-                }
-
+                totalRecvBytes += value.getSize();
                 double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
-                if (elapsed > 5) {
-                    System.out.printf(
-                            "stats: size: %s, rate: %sbps%n",
-                            humanReadableByteCountBin(totalBytes),
-                            humanReadableCountSI((long) (totalBytes * 8 / elapsed)));
-                    startTime = System.currentTimeMillis();
-                    totalBytes = 0;
-                }
+                System.out.printf(
+                        "stats: size: %s, rate: %sbps%n",
+                        humanReadableByteCountBin(totalRecvBytes),
+                        humanReadableCountSI((long) (totalRecvBytes * 8 / elapsed)));
             }
 
             @Override
@@ -69,27 +57,32 @@ public class GrpcExampleClient {
                 double elapsed = (System.currentTimeMillis() - startTime) / 1000.0;
                 System.out.printf(
                         "stats: size: %s, rate: %sbps%n",
-                        humanReadableByteCountBin(totalBytes),
-                        humanReadableCountSI((long) (totalBytes * 8 / elapsed)));
+                        humanReadableByteCountBin(totalRecvBytes),
+                        humanReadableCountSI((long) (totalRecvBytes * 8 / elapsed)));
                 System.out.println("on completed");
                 finishedLatch.countDown();
             }
-        });
-        requestObserverRef.set(observer);
-        startTime = System.currentTimeMillis();
+        };
+
+        StreamObserver<com.example.BiDirectionalExampleService.RequestCall> observer1 = service.connect(sharedObs);
+        StreamObserver<com.example.BiDirectionalExampleService.RequestCall> observer2 = service.connect(sharedObs);
         ExecutorService es = Executors.newSingleThreadExecutor();
         es.submit(() -> {
-            while (totalBytes < 1e9) {
+            startTime = System.currentTimeMillis();
+            StreamObserver<com.example.BiDirectionalExampleService.RequestCall> obs = observer1;
+            while (totalSentBytes < 1e9) {
                 byte[] b = new byte[1000000];
                 new Random().nextBytes(b);
                 ByteString data = ByteString.copyFrom(b);
                 RequestCall req = RequestCall.newBuilder().setData(data).build();
-                requestObserverRef.get().onNext(req);
-                totalBytes += b.length;
+                obs.onNext(req);
+                obs = (obs == observer1 ? observer2 : observer1);
+                totalSentBytes += b.length;
             }
         });
         finishedLatch.await();
-        observer.onCompleted();
+        observer1.onCompleted();
+        observer2.onCompleted();
     }
 
     // https://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java
